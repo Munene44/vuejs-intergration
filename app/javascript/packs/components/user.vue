e980f9b44001d7cff50fae928381a004ff98316d
nenekelvin <karvinmitchar@gmail.com>
2020-10-02 09:42:22 +0300

    datatabale script

--- app/javascript/packs/components/user.vue
+++ app/javascript/packs/components/user.vue
@@ -1,35 +1,13 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    sort-by="calories"
-    class="elevation-1"
-  >
+  <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
     <template v-slot:top>
-      <v-toolbar
-        flat
-      >
+      <v-toolbar flat color="white">
         <v-toolbar-title>My CRUD</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
+        <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
-            >
-              New Item
-            </v-btn>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
           </template>
           <v-card>
             <v-card-title>
@@ -39,55 +17,20 @@
             <v-card-text>
               <v-container>
                 <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Dessert name"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.first_name" label="First Name"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Calories"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.last_name" label="Last Name"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.phone" label="Phone"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.address" label="Address"></v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
@@ -95,58 +38,145 @@
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save(editedItem)">Save</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
       </v-toolbar>
     </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+    <template v-slot:item.action="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
+      <v-icon small @click="deleteItem(item)">delete</v-icon>
     </template>
     <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Reset
-      </v-btn>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
     </template>
   </v-data-table>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    dialog: false,
+    headers: [
+      {
+        text: "First Name",
+        align: "left",
+        sortable: false,
+        value: "first_name"
+      },
+      { text: "Last Name", value: "last_name" },
+      { text: "Email", value: "email" },
+      { text: "Phone", value: "phone" },
+      { text: "Address", value: "address" },
+      { text: "Actions", value: "action", sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      address: ""
+    },
+    defaultItem: {
+      name: "",
+      calories: "",
+      fat: "",
+      carbs: "",
+      protein: ""
+    }
+  }),
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+  
+  close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    }
+  }
+};
+</script>
+
+initialize() {
+return axios
+  .get("http://localhost:3000/users")
+  .then(response => {
+     console.log(response.data);
+     this.desserts = response.data;
+   })
+  .catch(e => {
+     console.log(e);
+  });
+}
+
+getUser(item) {
+  axios.get(`https://localhost:3000/${item.id}`)
+   .then(response => {
+     this.dessert = response.data;
+    })
+   .catch(error => {
+    console.log(error);
+    })
+}
+
+editItem(item) {
+  this.editedIndex = item.id;
+  this.editedItem = Object.assign({}, item);
+  this.dialog = true;
+}
+
+save(item) {
+  if (this.editedIndex > -1) {
+    axios
+      .put(`http://localhost:3000/users/${item.id}`, {
+        id: this.editedItem.id,
+        first_name: this.editedItem.first_name,
+        last_name: this.editedItem.last_name,
+        email: this.editedItem.email,
+        phone: this.editedItem.phone,
+        address: this.editedItem.address
+      })
+      .then(response => {
+       console.log(response);
+       this.initialize();
+      })
+      .catch(error => {
+       console.log(error);
+    });
+  } else {}
+  this.close();
+}
+
+else {
+ axios
+  .post(`http://localhost:3000/users/`, {
+     user: this.editedItem
+   })
+   .then(response => {
+     console.log(response);
+     console.log("Created!");
+     this.initialize();
+   })
+   .catch(error => {
+     console.log(error);
+ });
+}
